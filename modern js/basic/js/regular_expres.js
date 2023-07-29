b4c7af25771f0f66a0cfbaa4d627ab239aaf2c55
dipu157 <dipu157@gmail.com>
2023-07-29 13:01:44 +0600

    regular expression project done

--- modern js/basic/js/regular_expres.js
+++ modern js/basic/js/regular_expres.js
@@ -31,6 +31,41 @@ str = "Hello world";
 result = str.replace(re,"Hi"); // match kora take replace kore dey.
 //console.log(result);
 
+// Character Set using Brackets []
+re = /h[eai]llo/; // Must be one of them inside brackets
+re = /[HA]ello/i;
+re = /[^ha]ello/; // Anything except those inside brackets
+re = /^[ha]ello/; // Must start with h or a
+re = /[A-Z]ello/; // Start with Uper Case
+re = /^[A-Z]/; // First Letter must be upper case
+re = /^[a-z]/;
+re = /[A-Za-z]ello/;
+re = /[0-9]ello/;
+re = /^[0-9]ello/;
+re = /[^0-9]ello/; // Not digit
+re = /^[0-9][0-9][0-9]ello/; // three digits
+re = /^[0-9][0-9][0-9][0-9][0-9]/ // 5 digits
+
+// Braces {} - Quantifier
+re = /el{2}o/; // Must occur exactly 2 times
+re = /el{3}o/; 
+re = /hel{2,5}o/; // Range
+re = /hel{2,}o/; // At least 2 times
+
+// Parentheses () - Grouping
+re = /^([0-9]){5}/; // /^[0-9][0-9][0-9][0-9][0-9]/ // 5 digits
+re = /^([0-9]xy){4}/;
+
+// Bangladeshi Phone Number
+// total 11 digits
+re = /^01[0-9]{9}$/;
+re = /^+8801[0-9]{9}$/;
+
+
+str = "2xy3xy7xy8xy";
+str = "01788888888";
+str = "+8801811888889";
+
 reTest(re,str);
 function reTest(re,str){
     if(re.test(str)){
--- modern js/reg_expression_project/js/script.js
+++ modern js/reg_expression_project/js/script.js
@@ -1,7 +1,44 @@
 
+let exp,str;
 
 function promptInput(InputId){
     let userInput = prompt("Enter Your Input");
     console.log("InputId : "+InputId);
     console.log("userInput : "+userInput);
+
+    switch(InputId){
+        case 'postCode':
+            exp = /^\d{4}$/;
+            str = userInput;
+
+            if(exp.test(str)){
+                alert("Post Code is Valid");
+            }else{
+                alert("Invalid PostCode");
+            }
+            break;
+        
+        case 'phone':
+            exp = /^01[0-9]{9}$/;
+            str = userInput;
+
+            if(exp.test(str)){
+                alert("Phone Number is Valid");
+            }else{
+                alert("Invalid Phone Number");
+            }
+            break;
+
+        case 'email':
+            exp = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+            str = userInput;
+
+            if(exp.test(str)){
+                alert("Email is Valid");
+            }else{
+                alert("Invalid Email");
+            }
+            break;
+
+    }
 }
