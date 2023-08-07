84b3e684e41f58d301b7e2c842efd89a12b8b6b2
dipu157 <dipu157@gmail.com>
2023-08-07 21:00:46 +0600

    mysql DB configured

--- django practise/My_first_Project/settings.py
+++ django practise/My_first_Project/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'My_first_Project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bb_firstcrud',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',  # Or your MySQL host IP address
+        'PORT': '3306',       # Or your MySQL port
     }
 }
 
