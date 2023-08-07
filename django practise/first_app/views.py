3ced226ebe3a1cd5683e281220b50f1f9fb1e1fb
dipu157 <dipu157@gmail.com>
2023-08-08 00:00:11 +0600

    album insert

--- django practise/first_app/views.py
+++ django practise/first_app/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from first_app import forms
+from .models import Musician
 
 
 def index(request):
-    diction = {'title':"Home Page"}
+    musician_list = Musician.objects.order_by('first_name')
+    diction = {'title':"Home Page",'musician_list':musician_list}
     return render(request,'first_app/index.html',context=diction)
 
 
@@ -12,11 +14,30 @@ def album_list(request):
     return render(request,'first_app/index.html',context=diction)
 
 def musician_form(request):
-    diction = {'title':"Add Musician"}
+    form = forms.MusicianForm()
+
+    if request.method == 'POST':
+        form = forms.MusicianForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+
+
+    diction = {'title':"Add Musician",'musician_form':form}
     return render(request,'first_app/musician_form.html',context=diction)
 
 def album_form(request):
-    diction = {'title':"Add Album"}
+    form = forms.AlbumForm()
+
+    if request.method == 'POST':
+        form = forms.AlbumForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+
+    diction = {'title':"Add Album",'album_form':form}
     return render(request,'first_app/album_form.html',context=diction)
 
 
