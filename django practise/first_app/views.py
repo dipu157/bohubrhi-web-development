d197ad24b7df8f35ed3143a2a08045a40889602b
dipu157 <dipu157@gmail.com>
2023-08-07 13:07:04 +0600

    working with form bohubrihi

--- django practise/first_app/views.py
+++ django practise/first_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from first_app import forms
 
-# Create your views here.
+
+def form(request):
+    new_form = forms.user_form()
+    diction = {'test_form' : new_form, 'heading':"This is heading"}
+    return render(request, 'first_app/form.html', context=diction)
+
+
+
