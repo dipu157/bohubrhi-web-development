c0e6f8a45b49ce695deb901e1dec0f54cf2824c8
dipu157 <dipu157@gmail.com>
2023-08-07 15:52:10 +0600

    working with forms

--- django practise/first_app/forms.py
+++ django practise/first_app/forms.py
@@ -3,6 +3,6 @@ from django import forms
 
 class user_form(forms.Form):
 
-    user_name = forms.CharField()
+    user_name = forms.CharField(label="Full Name",widget=forms.TextInput(attrs={'Placeholder':"Enter Name"}))
 
-    user_email = forms.EmailField()
+    user_dob = forms.DateField(label="Dat of Birth", widget=forms.TextInput(attrs={'type':'date'}))
