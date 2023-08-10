e157d5ad84fe949884e738c7b6f35ff49550d855
dipu157 <dipu157@gmail.com>
2023-08-10 23:23:02 +0600

    index page create done

---
+++ django/second project/Login_app/forms.py
@@ -0,0 +1,8 @@
+from django import  forms
+from django.contrib.auth.models import User
+from Login_app.models import UserInfo
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('username','email','password')
