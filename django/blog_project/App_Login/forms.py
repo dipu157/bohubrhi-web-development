8a5a46e8636828dd6b56762df8ddebe9333c5605
dipu157 <dipu157@gmail.com>
2023-08-17 16:51:40 +0600

    working on user profile

--- django/blog_project/App_Login/forms.py
+++ django/blog_project/App_Login/forms.py
@@ -13,4 +13,4 @@ class SignUpForm(UserCreationForm):
 class UserProfileChange(UserChangeForm):
     class Meta:
         model = User
-        fields = ('username','email','first_nme','last_name')
+        fields = ('username','email','first_name','last_name')
--- django/blog_project/App_Login/urls.py
+++ django/blog_project/App_Login/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('login/', views.login_page, name='login'),
     path('profile/', views.profile, name='profile'),
     path('logout/', views.logout_user, name='logout'),
+    path('change_profile/', views.user_change, name='change_profile'),
+    path('password/', views.pass_change, name='pass_change'),
 ]
